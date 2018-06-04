d41ff2ab2eb321987ce3c9931655b0c3ddf736af
Matt Zahorik <mzahorik@bottomline.com>
2018-06-04 16:21:18 -0400

    Add persist and fallbackPersistence to the virtual server dictionary

--- f5_cccl/resource/ltm/virtual.py
+++ f5_cccl/resource/ltm/virtual.py
@@ -63,7 +63,9 @@ class VirtualServer(Resource):
                       pool=None,
                       policies=list(),
                       profiles=list(),
-                      rules=list())
+                      rules=list(),
+                      persist=None,
+                      fallbackPersistence=None)
 
     def __init__(self, name, partition, default_route_domain, **properties):
         """Create a Virtual server instance."""
